306bf9d540b5d63ecb8ab03785180312d992fa3a
Syafik <syafiklijtk04@gmail.com>
2014-02-06 16:07:32 +0700

    Update codes

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140110092040) do
-
-  create_table "references", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "tests", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20140106070100) do
 
   create_table "transactions", :force => true do |t|
     t.datetime "date"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.find_or_create_by_email!(:email => 'pushjaw@yahoo.com', :password => '123456789', :password_confirmation => '123456789')
+User.find_or_create_by_email!(:email => 'admin@pushjaw.com', :password => '1Pushjaw')
