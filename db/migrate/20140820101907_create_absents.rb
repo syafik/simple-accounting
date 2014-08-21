59d87b517a12a6dc3a56c50f1a6c12cd0b654d8d
Herlangaws <herlangaw.wicaksono@gmail.com>
2014-08-21 11:34:14 +0700

    fixed creat_absents migration on int to be integer

--- db/migrate/20140820101907_create_absents.rb
+++ db/migrate/20140820101907_create_absents.rb
@@ -2,8 +2,8 @@ class CreateAbsents < ActiveRecord::Migration
   def change
     create_table :absents do |t|
       t.date :date
-      t.int :user_id
-      t.int :categories
+      t.integer :user_id
+      t.integer :categories
       t.string :description
 
       t.timestamps
