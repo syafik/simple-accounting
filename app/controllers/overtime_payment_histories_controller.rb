6546e05052d5b229fc6a02959b631e3e48aa2532
Herlangaws <herlangaw.wicaksono@gmail.com>
2014-09-22 12:40:08 +0700

    added CRUD to overtime payment histories module

--- app/controllers/overtime_payment_histories_controller.rb
+++ app/controllers/overtime_payment_histories_controller.rb
@@ -1,6 +1,76 @@
 
 class OvertimePaymentHistoriesController < ApplicationController
+	before_filter :get_user, :only => [:new, :create, :edit, :update]
 	def index
-		@oph = OvertimePaymentHistory.order("date asc")
+		@oph = OvertimePaymentHistory.order("applicable_date asc")
 	end
-end
+
+	def show
+		@oph = OvertimePaymentHistory.find(params[:id])
+
+
+		respond_to do |format|
+		      format.html  #show.html.erb
+		      format.json { render json: @oph }
+		    end
+		  end
+
+		  def new
+		  	@oph = OvertimePaymentHistory.new
+
+		  	respond_to do |format|
+	      format.html # new.html.erb
+	      format.json { render json: @oph }
+	    end
+	  end
+
+	  def create
+	  	params[:overtime_payment_history][:applicable_date] = DateTime.strptime(params[:overtime_payment_history][:applicable_date], "%m/%d/%Y").to_date
+	  	@oph = OvertimePaymentHistory.new(params[:overtime_payment_history])
+	  	p "================="
+	  	p params
+	  	p @oph
+
+	  	respond_to do |format|
+	  		if @oph.save
+	  			format.html { redirect_to @oph, notice: 'Salary history was successfully created.' }
+	  			format.json { render json: @oph, status: :created, location: @oph }
+	  		else
+	  			format.html { render action: "new" }
+	  			format.json { render json: @oph.errors, status: :unprocessable_entity }
+	  		end
+	  	end
+	  end
+
+	  def edit
+	  	@oph = OvertimePaymentHistory.find(params[:id])
+	  end
+
+	  def update
+	  	@oph = OvertimePaymentHistory.find(params[:id])
+
+	  	respond_to do |format|
+	  		if @oph.update_attributes(params[:overtime_payment_history])
+	  			format.html { redirect_to @oph, notice: 'Salary history was successfully updated.' }
+	  			format.json { head :no_content }
+	  		else
+	  			format.html { render action: "edit" }
+	  			format.json { render json: @oph.errors, status: :unprocessable_entity }
+	  		end
+	  	end
+	  end
+
+	  def set_activation
+	  	@oph = OvertimePaymentHistory.find(params[:id])
+	  	@oph.update_attributes(:activate => true)
+
+	  	if OvertimePaymentHistory.update_all("activate = false", "id <> #{ params[:id] } AND user_id = #{@oph.user_id}" )
+	  		redirect_to overtime_payment_histories_path
+	  	end
+	  end
+
+	  private
+	  def get_user
+	  	@users = User.all.map {|user| [user.email, user.id]}
+	  end
+	end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ SimpleAccounting::Application.routes.draw do
   resources :salaries
   resources :manage_users
 
-  resources :overtime_payment_histories
+  
   
 
 
@@ -16,8 +16,8 @@ SimpleAccounting::Application.routes.draw do
 
   # get "allowances/index"
 
-match "/loan_payments/:id/new" => "loan_payments#new",   :as => "new_loan_payments",  :via => :get
-match "/loan_payments/:id/edit" => "loan_payments#edit",   :as => "edit_loan_payments",  :via => :get
+  match "/loan_payments/:id/new" => "loan_payments#new",   :as => "new_loan_payments",  :via => :get
+  match "/loan_payments/:id/edit" => "loan_payments#edit",   :as => "edit_loan_payments",  :via => :get
 
   resources :allowance_categories do
     resources :allowance_sub_categories
@@ -26,6 +26,12 @@ match "/loan_payments/:id/edit" => "loan_payments#edit",   :as => "edit_loan_pay
   resources :allowance_claim_transactions do
     collection do
       get :set_approval
+    end
+  end
+
+  resources :overtime_payment_histories do
+    member do
+      get :set_activation
     end
   end
 
