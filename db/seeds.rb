2b252b3017ec086365b9efc8207b01ee845b86c3
Herlangaws <herlangaw.wicaksono@gmail.com>
2014-09-23 11:28:37 +0700

    modified role as 2 types: Admin and Non Admin(User)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 p "creaet user"
-User.find_or_create_by_email!(:email => 'admin@pushjaw.com', :first_name => "Admin", :last_name => "pushjaw", :telephone => "0987654321", :address => "mig 1", :role_id => 2, :password => '1Pushjaw')
+User.find_or_create_by_email!(:email => 'admin@pushjaw.com', :first_name => "Admin", :last_name => "pushjaw", :telephone => "0987654321", :address => "mig 1", :role_id => 2, :password => '1Pushjaw', :birth_date => "1993-04-09")
+User.find_or_create_by_email!(:email => 'herlangga@pushjaw.com', :first_name => "Heralngga", :last_name => "pushjaw", :telephone => "0987654321", :address => "mig 1", :role_id => 1, :password => '12345678', :birth_date => "1993-04-09")
+Role.find_or_create_by_name!(:name => 'non admin')
+Role.find_or_create_by_name!(:name => 'admin')
+
 p "finish"
