70109ff98a9ca59a135f0eb82f1aba1629201aea
Herlangaws <herlangaw.wicaksono@gmail.com>
2014-10-02 12:20:55 +0700

    add redirect to setting module

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,6 +8,7 @@ class SettingsController < ApplicationController
 		Setting[:startlimitdaytime] = params[:startlimitdaytime]
 		Setting[:startlimitnighttime] = params[:startlimitnighttime]
 		Setting[:maxovertimeperday] = params[:maxovertimeperday]
+		redirect_to overtime_setting_path
 	end
 
 end
