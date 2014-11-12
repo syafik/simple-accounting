3a933051154c8b65c6ce6850ed425feb0ad12aab
lenovo <lenovo@lenovo-Ideapad-Z460.(none)>
2014-11-12 20:47:38 +0700

    rincian_piutang

--- app/helpers/account_receivables_helper.rb
+++ app/helpers/account_receivables_helper.rb
@@ -3,7 +3,9 @@ module AccountReceivablesHelper
 		credits = []
 		AccountReceivable.where(parent_id: nil).collect do |p|
 			debit = p.children.sum(&:debit)
+			if p.credit - p.debit != 0
 			credits << ["#{p.title} sisa cicilan Rp.#{p.credit - debit}, p.id"]
+			end
 		end
 		credits
 	end
