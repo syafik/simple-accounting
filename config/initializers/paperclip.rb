f1dc971a1e50ae296acf14c11277ab9a650498c9
agung43 <sniveragung43@gmail.com>
2016-03-29 14:04:19 +0700

    date

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,11 @@
+Paperclip.interpolates :year do |attachment, style|
+  attachment.instance.created_at.year
+end
+
+Paperclip.interpolates :month do |attachment, style|
+  attachment.instance.created_at.month
+end
+
+Paperclip.interpolates :day do |attachment, style|
+  attachment.instance.created_at.day
+end
