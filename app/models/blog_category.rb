c547bde0ebee0e47d84ecb5dedb8d3703826c986
agung43 <sniveragung43@gmail.com>
2016-05-03 14:56:01 +0700

    add friendly id to model

--- app/models/blog_category.rb
+++ app/models/blog_category.rb
@@ -1,6 +1,7 @@
 class BlogCategory < ActiveRecord::Base # :nodoc:
   attr_accessible :name
-
+  extend FriendlyId
+  friendly_id :name, use: :slugged
   has_many :categoriesblogs
   has_many :blogs, :through => :categoriesblogs
 
